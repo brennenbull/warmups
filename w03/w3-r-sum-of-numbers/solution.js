4af496fcb1c8a545583aec5b11943a8576a0735d
Brennen Barret Bull <brennenbull@Brennens-MacBook-Pro.local>
2017-06-14 10:12:10 -0600

    w07 part 1

--- w03/w3-r-sum-of-numbers/solution.js
+++ w03/w3-r-sum-of-numbers/solution.js
@@ -1,10 +1,10 @@
-/* Given two integers, which can be positive and negative, 
+/* Given two integers, which can be positive and negative,
 find the sum of all the numbers between and including a and b,
 and return the sum. If both numbers are equal return a or b.
 
 Note! a and b are not ordered!
 
-Example: 
+Example:
 getSum(1, 0) == 1   // 1 + 0 = 1
 getSum(1, 2) == 3   // 1 + 2 = 3
 getSum(0, 1) == 1   // 0 + 1 = 1
@@ -13,32 +13,50 @@ getSum(-1, 0) == -1 // -1 + 0 = -1
 getSum(-1, 2) == 2  // -1 + 0 + 1 + 2 = 2
 */
 
+// function getSum( a, b ) {
+//   console.log(a,b,"YOLO")
+//   if (a === b) {
+//     return a
+//   }
+//
+//   var accumulator = 0;
+//
+//   if (a < b) {
+//     for (var i = a ; i <= b ; i++) {
+//       accumulator += a
+//       // a++
+//     }
+//   } else {
+//     for (var i = b ; i <= a ; i++) {
+//       accumulator += b
+//       // b++
+//     }
+//   }
+//   return accumulator
+// }
+// END FUNCTION
 function getSum( a, b ) {
-  console.log(a,b,"YOLO")
-  if (a === b) {
-    return a
-  }
-
-  var accumulator = 0;
-
-  if (a < b) {
-    for (var i = a ; i <= b ; i++) {
-      accumulator += a
-      // a++
+  var total = 0;
+  if(a < b){
+    for (let i = a; i <= b; i++) {
+      total += a;
+      a++
     }
-  } else {
-    for (var i = b ; i <= a ; i++) {
-      accumulator += b
-      // b++
+    return total;
+  } else if(a > b){
+    for (let i = b; i <= a; i++) {
+      total += b;
+      b++
     }
+    return total;
+  } else if(a === b){
+    return a;
   }
-  return accumulator
-} // END FUNCTION
+}
+
 
 
 module.exports = {
   getSum:getSum,
   attendance:"WORD UP"
 }
-
-
--- w07-http-verb-warmup/partOne.js
+++ w07-http-verb-warmup/partOne.js
@@ -1,8 +1,32 @@
 // 🐾🐾🐾 PART ONE 🐾🐾🐾
 
 var parseRequest = function (inputString) {
-  return;
-} // END FUNCTION
+  var inputArr = inputString.split('\n');
+  var requestObj ={
+    headers: {},
+    verb: null,
+    path: null,
+    protocol: null,
+  };
+  var mainArr = [];
+  inputArr.forEach((ele)=>{
+    mainArr.push(ele.split(':'));
+  });
+  for (var i = 0; i < mainArr.length; i++) {
+    if(mainArr[i][0].indexOf("GET") !== -1){
+      var verbArr = mainArr[i][0].split(' ');
+      requestObj.verb = verbArr[0];
+      requestObj.path = verbArr[1];
+      requestObj.protocol =verbArr[2];
+    }else{
+      var keys = mainArr[i][0];
+      var value = mainArr[i][1];
+      requestObj.headers[keys] = value;
+    }
+  }
+  return requestObj;
+};
+ // END FUNCTION
 
 module.exports = {
   parseRequest: parseRequest,
